d991e01ddcdb3081f6a59485c36719451bbfbbb6
ameenshmeen <aamhjx@gmail.com>
2012-06-20 19:13:52 -0300

    Update master

--- BayesianLogic/BayesClassifier.cs
+++ BayesianLogic/BayesClassifier.cs
@@ -5,14 +5,25 @@ using System.Text;
 
 namespace BayesianLogic
 {
-	class BayesClassifier
+	class BayesClassifier 
 	{
+		private float training_Factor = training.Instance;
+		
+		public void Initialize();
+		{
+			base.Initialize();
+		}
 
-		public Bayesian(int n_dimensions) 
+		protected void Dispose(bool disposing)
+	        {
+	            base.Dispose(disposing);
+	        }
+		
+		public int Bayesian(int n_dimensions) 
 		{
 			for (int i = 0; i <= n_dimensions; i++)
 			//TODO: calculate 
 		}
-
+		
 	}
 }
