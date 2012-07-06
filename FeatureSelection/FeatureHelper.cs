3056ff799a4f4044d7f78c78cba9e7d50132e163
Ameen Modhafar <aamhjx@gmail.com>
2012-07-05 22:53:35 -0400

    Commiting feature selection

---
+++ FeatureSelection/FeatureHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;	
+
+namespace FeatureSelection : 
+
+public class FeatureHelper 
+{
+    public FeatureHelper
+    {
+
+    }
+
+    public void Initilizating()
+    {
+
+    }
+
+    public void Disposing()
+    {
+
+    }
+
+	public void ExtractFeatures()
+	{
+        struct ExerciseSet =  new struct;
+        struct DataSet=  new struct;
+
+        
+	}
+}
